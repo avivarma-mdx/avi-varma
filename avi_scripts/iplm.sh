cc1fe862958615839f7b68fa5842674e0c3d6273
avivarma-mdx <avi.varma@perforce.com>
2025-04-22 02:43:51 -0700

    added logic to delete venv first when building iplm.

--- avi_scripts/iplm.sh
+++ avi_scripts/iplm.sh
@@ -32,6 +32,7 @@ set_java_home_to_jdk11() {
 }
 
 build_server() {
+    rm -rf /home/avi.varma/develop/tau/venv
     cd /home/avi.varma/develop/tau/iplm
     ./gradlew clean
     ./gradlew :piserver-application:installDist
